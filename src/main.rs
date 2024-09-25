b045d670053758786c6eb25a2d1d260ebdde1186
maximopalopoli <mpalopoli@fi.uba.ar>
2024-09-25 15:19:18 -0300

    Add documentation comments on main file

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ const SQUARE_LENGTH:f32 = 20.0;
 const SQUARE_OFFSET:f32 = 5.0;
 const ARROW_WIDTH: f32 = 5.5;
 
+/// Prints the numbers of the Y axis, the Y and the arrow 
 fn draw_y_axis(grid_width: usize){
     for i in 0..grid_width {
         draw_text(i.to_string().as_str(), FLOOR_X + MATRIX_STEP_X + (i as f32)*20.0 + (i as f32)*SQUARE_OFFSET, FLOOR_Y + 20.0, TEXT_FONT_SIZE, BLACK);
@@ -24,28 +25,27 @@ fn draw_y_axis(grid_width: usize){
     draw_text("Y", common_x1 + 15.0 , FLOOR_Y + 20.0, TEXT_FONT_SIZE, BLACK);
 }
 
+/// Prints the numbers of the X axis, the X and the arrow 
 fn draw_x_axis(grid_height: usize){
     for i in 0..grid_height {
         draw_text(i.to_string().as_str(), FLOOR_X, FLOOR_Y + MATRIX_STEP_Y + 20.0 + (i as f32)*20.0 + (i as f32)*SQUARE_OFFSET, TEXT_FONT_SIZE, BLACK);
     }
-    let common_y1 = FLOOR_Y + MATRIX_STEP_Y + (grid_height as f32)*20.0 + (grid_height as f32)*SQUARE_OFFSET;
-    draw_line(FLOOR_X + 10.0, common_y1, FLOOR_X + 10.0, common_y1 + 12.5, 2.0, BLACK);
-    draw_line(FLOOR_X + 5.0, common_y1 + ARROW_WIDTH, FLOOR_X + 10.0, common_y1 + 12.5, 2.0, BLACK);
-    draw_line(FLOOR_X + 15.0, common_y1 + ARROW_WIDTH, FLOOR_X + 10.0, common_y1 + 12.5, 2.0, BLACK);
+    let common_y1 = FLOOR_Y + MATRIX_STEP_Y + (grid_height as f32)*20.0 + (grid_height as f32)*SQUARE_OFFSET - 5.0;
+    draw_line(FLOOR_X + 10.0, common_y1 + 5., FLOOR_X + 10.0, common_y1 + 17.5, 2.0, BLACK);
+    draw_line(FLOOR_X + 5.0, common_y1 + 5. + ARROW_WIDTH, FLOOR_X + 10.0, common_y1 + 17.5, 2.0, BLACK);
+    draw_line(FLOOR_X + 15.0, common_y1 + 5. + ARROW_WIDTH, FLOOR_X + 10.0, common_y1 + 17.5, 2.0, BLACK);
     draw_text("X", FLOOR_X + 2.5, FLOOR_Y + MATRIX_STEP_Y + 25.5 + (grid_height as f32)*20.0 + (grid_height as f32)*SQUARE_OFFSET, TEXT_FONT_SIZE, BLACK);
-
 }
 
-
+/// Draws the grid, when the cells are alive draws a black square, and if not, a blank one
 fn draw_grid(grid: &mut Grid) {
-
     let (grid_width, grid_height) = grid.dimensions();
 
     draw_y_axis(grid_width);
 
     draw_x_axis(grid_height);
 
-    // Draw cells
+    // To draw the cells
     for x in 0..grid_width {
         for y in 0..grid_height {
             if grid.at(x, y) {
@@ -57,6 +57,10 @@ fn draw_grid(grid: &mut Grid) {
     }
 }
 
+/// Here remains the logic of the windows of options on the right side of the screen.
+/// The main window handles the advance options and the reset one. The second handles 
+/// the setting of the time between generations, and the third shows the last grid that
+/// had a change of state (and handles the change)
 pub fn windows_logic(grid: &mut Grid, generation: &mut i32, auto_advance: &mut bool, chosen_cell: &mut (usize, usize), time_sleep: &mut f32) {
     let (grid_width, grid_height) = grid.dimensions();
         
@@ -95,12 +99,10 @@ pub fn windows_logic(grid: &mut Grid, generation: &mut i32, auto_advance: &mut b
     .label("Grid change")
     .titlebar(true)
     .ui(&mut root_ui(), |ui| {
-        //let (mouse_x, mouse_y) = mouse_position();
-        //ui.label(None, &format!("Chosen cell: {} {}", mouse_x, mouse_y));
-
         if is_mouse_button_pressed(MouseButton::Left) {
             let (mouse_x, mouse_y) = mouse_position();
-            // Notar que los limites mayores de x e y dependen del tamaño de la tabla
+            // Note: the literals on the upper bounds are related to the common_x and common_y values
+            // of the drawing axes functions. Should be replaced in future versions
             if mouse_x > 30. && mouse_x < 525. && mouse_y > 70. && mouse_y < 565.{
                 let x_position = mouse_x - 30.0;
                 let y_position = mouse_y - 70.0;
